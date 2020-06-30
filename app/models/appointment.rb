e5d0210a821005a6d9f89865c696310ee0aa3f2b
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-30 09:41:45 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+  
+  def pretty_datetime
+    self.appointment_datetime.strftime("%B %d, %Y at %k:%M")
+  end
+  
 end
